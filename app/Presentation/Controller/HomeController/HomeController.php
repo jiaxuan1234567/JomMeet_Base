9c8592f81007bc885f222ba9068bd3485594ea83
Jiaying <jiayingyong8@gmail.com>
2025-04-15 17:57:09 +0800

    Fix: routing

--- app/Presentation/Controller/HomeController/HomeController.php
+++ app/Presentation/Controller/HomeController/HomeController.php
@@ -1,9 +1,19 @@
 <?php
+define('ROOTPATH', __DIR__);
+require('fileResgister.php'); // The file that store the route
 require_once __DIR__ . '/../../../BusinessLogic/model/homeModel/homeModel.php';
 require_once __DIR__ . '/../../../Presentation/view/homeView/homeView.php';
 
 class homeController
 {
+
+    private $path;
+
+    public function __construct()
+    {
+       $this->path = getFilePath("home");
+    }
+
     public function index()
     {
         // session_start();
@@ -21,5 +31,10 @@ class homeController
             $view->render();
         }
         $view->footer();
+    }
+
+    public function redirect($key)
+    {
+        return $this->path[$key];
     }
 }
