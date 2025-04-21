684ab55e81ebd70c2ef0b3f8ebf9450d4ed4872d
cloudo <mantik3333@gmail.com>
2025-04-21 21:40:51 +0800

    Migrating files

--- app/Presentation/Controller/GatheringController/GatheringController.php
+++ app/Presentation/Controller/GatheringController/GatheringController.php
@@ -1,35 +1,65 @@
 <?php
-require_once "../../BusinessLogic/Model/GatheringModel.php";
+// require_once ROOTPATH . '/fileRegister.php';
+// require_once getFilePath('Database');
+// require_once getFilePath('GatheringModel');
+require_once(ROOTPATH . '/fileRegister.php');
+require ROOTPATH . '/BusinessLogic/Model/GatheringModel/GatheringModel.php';
+require_once '../app/Database.php';
 
 class GatheringController
 {
     private $gatheringModel;
+    private $path;
 
     public function __construct()
     {
-        $this->gatheringModel = new GatheringModel();
+        $this->path = getFilePath("gathering");
+        try {
+            $db = DatabaseTest::getConnection();
+            $this->gatheringModel = new GatheringModel($db);
+        } catch (Exception $e) {
+            error_log("Error in GatheringController constructor: " . $e->getMessage());
+            throw $e;
+        }
     }
 
-    public function createGathering()
+    public function redirect($key)
     {
-        if ($_SERVER["REQUEST_METHOD"] === "POST") {
-            $hostId = $_POST['host_id'];
-            $location = $_POST['location'];
-            $theme = $_POST['theme'];
-            $maxParticipants = $_POST['max_participants'];
-            $description = $_POST['description'];
-            $dateTime = $_POST['date_time'];
+        return $this->path[$key];
+    }
 
-            $result = $this->gatheringModel->createGathering($hostId, $location, $theme, $maxParticipants, $description, $dateTime);
-
-            if ($result) {
-                header("Location: success.php");
-            } else {
-                echo "Error creating gathering.";
+    public function listGatherings()
+    {
+        try {
+            $gatherings = $this->gatheringModel->getAllGatherings();
+            if ($gatherings === false) {
+                error_log("getAllGatherings returned false");
+                return [];
             }
+            return $gatherings;
+        } catch (Exception $e) {
+            error_log("Error in listGatherings: " . $e->getMessage());
+            return [];
+        }
+    }
+
+    public function viewGathering($id)
+    {
+        try {
+            return $this->gatheringModel->getGatheringById($id);
+        } catch (Exception $e) {
+            error_log("Error in viewGathering: " . $e->getMessage());
+            return null;
+        }
+    }
+
+    public function action()
+    {
+        try {
+            return $this->gatheringModel->handleAction();
+        } catch (Exception $e) {
+            error_log("Error in action: " . $e->getMessage());
+            return null;
         }
     }
 }
-
-$controller = new GatheringController();
-$controller->createGathering();
