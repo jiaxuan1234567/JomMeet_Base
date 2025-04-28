02bbf70ea32a0e5786a72c3c4a667a34523ac58d
noobrs <vinsenyong8763@gmail.com>
2025-04-28 08:10:15 +0800

    delete extra div

--- app/Presentation/View/GatheringView/gathering-list.php
+++ app/Presentation/View/GatheringView/gathering-list.php
@@ -44,35 +44,38 @@ $userid = 1;
 
                     <?php $notJoined = $controller->verifyUserInGathering($userid, $gathering['gatheringID']); ?>
 
-                    <?php if ($gathering['currentParticipant'] < $gathering['maxParticipant'] 
-                    || $controller->isBeforeStartTime($gathering['gatheringID']) 
-                    || !$controller->isNewGatheringConflicting($userid, $gathering['gatheringID'])): ?>
-                        <?php if ($notJoined): ?>
-                            <div class="col-md-6">
-                                <div class="d-flex border rounded shadow-sm p-2 bg-white">
-                                    <img src="<?= $asset->getFilePath('dinner') ?>" alt="Dinner" class="rounded" style="width: 120px; height: auto; object-fit: cover;">
-                                    <div class="ms-3 d-flex flex-column justify-content-between flex-grow-1">
-                                        <div>
-                                            <strong><?php echo htmlspecialchars($gathering['theme']); ?></strong><br>
-                                            <small>Date: <?php echo htmlspecialchars($gathering['date']); ?></small><br>
-                                            <small>Time: <?php echo date('g:i A', strtotime($gathering['startTime'])); ?></small><br>
-                                            <small>End Time: <?php echo date('g:i A', strtotime($gathering['endTime'])); ?></small><br>
-                                            <small>Preference: <?php echo htmlspecialchars($gathering['preference']); ?></small><br>
-                                            <p class="fs-7 fw-bold mb-1 mt-2">Current Pax</p>
-                                            <p class="fs-7 mb-0"><?php echo htmlspecialchars($gathering['currentParticipant']) . '/' . htmlspecialchars($gathering['maxParticipant']); ?></p>
-                                        </div>
-                                        <div class="mt-2">
-                                            <a data-get="/gathering/view/<?= htmlspecialchars($gathering['gatheringID']) ?>" class="btn btn-primary w-100">View Details</a>
-                                        </div>
+                <?php if (
+                    $gathering['currentParticipant'] < $gathering['maxParticipant']
+                    && $controller->isBeforeStartTime($gathering['gatheringID'])
+                    && !$controller->isNewGatheringConflicting($userid, $gathering['gatheringID'])
+                ): ?>
+                    <?php if ($notJoined): ?>
+                        <div class="col-md-6">
+                            <div class="d-flex border rounded shadow-sm p-2 bg-white">
+                                <img src="<?= $asset->getFilePath('dinner') ?>" alt="Dinner" class="rounded" style="width: 120px; height: auto; object-fit: cover;">
+                                <div class="ms-3 d-flex flex-column justify-content-between flex-grow-1">
+                                    <div>
+                                        <strong><?php echo htmlspecialchars($gathering['theme']); ?></strong><br>
+                                        <small>Date: <?php echo htmlspecialchars($gathering['date']); ?></small><br>
+                                        <small>Time: <?php echo date('g:i A', strtotime($gathering['startTime'])); ?></small><br>
+                                        <small>End Time: <?php echo date('g:i A', strtotime($gathering['endTime'])); ?></small><br>
+                                        <small>Venue: <?php echo htmlspecialchars($gathering['preference']); ?></small><br>
+                                        <p class="fs-7 fw-bold mb-1 mt-2">Current Pax</p>
+                                        <p class="fs-7 mb-0"><?php echo htmlspecialchars($gathering['currentParticipant']) . '/' . htmlspecialchars($gathering['maxParticipant']); ?></p>
+                                    </div>
+                                    <div class="mt-2">
+                                        <a data-get="/gathering/view/<?= htmlspecialchars($gathering['gatheringID']) ?>" class="btn btn-primary w-100">View Details</a>
                                     </div>
                                 </div>
                             </div>
-                        <?php endif; ?>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </div>
-        <?php endif; ?>
-    </div>
+                        </div>
+        </div>
+    <?php endif; ?>
+<?php endif; ?>
+<?php endforeach; ?>
+</div>
+<?php endif; ?>
+</div>
 
     <?php include __DIR__ . '../../../View/HomeView/footer.php'; ?>
 </body>
