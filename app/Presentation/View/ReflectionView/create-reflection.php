d86c4808ae4314f7aca886dc72fb2b3a83b1db9b
waikin0109 <wongwk0109@gmail.com>
2025-05-01 21:46:10 +0800

    normal validation create.php

--- app/Presentation/View/ReflectionView/create-reflection.php
+++ app/Presentation/View/ReflectionView/create-reflection.php
@@ -14,33 +14,36 @@ unset($_SESSION['old']);
 
 
 <div class="main d-flex flex-column min-vh-100">
-    <div class="container-fluid py-3">
-        <h4>
-            <a href="/reflection">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
-                </svg>
-            </a>
-            Create Self-Reflection
-        </h4>
+    <div class="container-fluid py-3 d-flex align-items-center">
+        <a href="/reflection" class="me-3">
+            <i class="bi bi-arrow-left text-black" style="font-size: 1.55rem;"></i>
+        </a>
+        <h4 class="mb-0">Create Self-Reflection</h4>
     </div>
     <div class="container mb-5">
         <div class="container border border-2 rounded-3 m-2 align-content-center" style="background-color: rgba(222,236,255,68); border-color:#0077CC !important;">
             <div class="container d-flex align-item-center justify-content-center">
                 <form method="post" action="/reflection/create" class="form">
+                    <h1></h1>
                     <div class="my-3">
-                        <h4><label for="date">Date</label></h4>
-                        <input type="text" id="reflectionDate" name="reflectionDate" value="<?= date("Y-m-d h:i") ?>" style="width:500px;" readonly>
+                        <h4 style="font-size: 30px;"><label for="date"><b>Date</b></label></h4>
+                        <input type="text" class="ps-2 bg-light-subtle border border-black border-2 rounded-3"id="reflectionDate" name="reflectionDate" value="<?= date("Y-m-d H:i") ?>" style="width:500px;" readonly>
                     </div>
                     
                     <div class="my-3">
-                        <h4><label for="title">Title</label></h4>
-                        <input type="text" id="reflectionTitle" name="reflectionTitle" maxlength="255" placeholder="Write Your Self-Reflection Title Here!" style="width:500px;">
+                        <h4 style="font-size: 30px;"><label for="title"><b>Title</b></label></h4>
+                        <input type="text" class="ps-2 border border-black border-2 rounded-3" id="reflectionTitle" name="reflectionTitle" placeholder="Write Your Self-Reflection Title Here!" value="<?= htmlspecialchars($old['reflectionTitle'] ?? '') ?>" style="width:500px;">
+                        <?php if (!empty($errors['reflectionTitle'])): ?>
+                            <div class="text-danger"><?= $errors['reflectionTitle'] ?></div>
+                        <?php endif; ?>
                     </div>
 
                     <div class="my-3">
-                    <h4><label for="content">Content</label></h4>
-                        <textarea id="reflectionContent" name="reflectionContent" placeholder="Share how was your day!" style="width:500px;height:200px;"></textarea>
+                        <h4 style="font-size: 30px;"><label for="content"><b>Content</b></label></h4>
+                        <textarea class="ps-2 border border-black border-2 rounded-3" id="reflectionContent" name="reflectionContent" placeholder="Share how was your day!" style="width:500px;height:200px;"><?= htmlspecialchars($old['reflectionContent'] ?? '') ?></textarea>
+                        <?php if (!empty($errors['reflectionContent'])): ?>
+                            <div class="text-danger"><?= $errors['reflectionContent'] ?></div>
+                        <?php endif; ?>
                     </div>
 
                     <section class="d-flex justify-content-center my-3">
--- app/Presentation/View/ReflectionView/view-reflection.php
+++ app/Presentation/View/ReflectionView/view-reflection.php
@@ -6,23 +6,18 @@ require_once __DIR__ . '/../HomeView/header.php';
 ?>
 
 <div class="main d-flex flex-column min-vh-100">
-    <div class="container-fluid py-3">
-        <h4>
-            <a href="/reflection">
-                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
-                </svg>
-            </a>
-            View Self-Reflection
-        </h4>
+    <div class="container-fluid py-3 d-flex align-items-center">
+        <a href="/reflection" class="me-3">
+            <i class="bi bi-arrow-left text-black" style="font-size: 1.55rem;"></i>
+        </a>
+        <h4 class="mb-0">View Self-Reflection</h4>
     </div>
 
     <div class="container mb-5">
         <?php if (!$reflectionViewed): ?>
             <section class="text-center my-5">
-                <!-- Spline 3D Object -->
-                <spline-viewer url="https://prod.spline.design/VDuiMvkxF4DPHkAv/scene.splinecode" class="spline-model"></spline-viewer>
-                <div>
+                <img src="/asset/bubble.png">
+                <div class="mt-5">
                     <h4 class="fw-bold">Sorry, the reflection you are looking for was not found.</h4>
                     <a href="/reflection" class="btn hero-btn px-4 py-2">Go Back Reflection List Page</a>
                 </div>
