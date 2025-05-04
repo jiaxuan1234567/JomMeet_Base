d1bae872b4190453c23574f168051a7e5df081c0
waikin0109 <wongwk0109@gmail.com>
2025-05-04 17:57:40 +0800

    edit ui done

--- app/Presentation/View/ReflectionView/edit-reflection.php
+++ app/Presentation/View/ReflectionView/edit-reflection.php
@@ -22,21 +22,21 @@ require_once __DIR__ . '/../HomeView/header.php';
     <div class="container mb-5">
         <div class="container border border-2 rounded-3 m-2 align-content-center" style="background-color: rgba(222,236,255,68); border-color:#0077CC !important;">
             <div class="container d-flex align-item-center justify-content-center">
-            <form method="post" action="/reflection/edit/<?php echo $reflectionSelected['selfreflectID']; ?>" class="form" id="selfReflectionForm">
+            <form method="post" action="/reflection/edit/<?php echo $reflectionSelected['selfreflectID']; ?>" class="form" id="selfReflectionForm" style="width:500px;">
                     <div class="my-3">
                         <h4 style="font-size: 30px;"><label for="date">Date</label></h4>
-                        <input type="text" class="ps-2 bg-light-subtle border border-black border-2 rounded-3" id="reflectionDate" name="reflectionDate" value="<?php echo date('Y-m-d H:i', strtotime($reflectionSelected['date'] ?? '')); ?>" style="width:500px;" readonly>
+                        <input type="text" class="w-100 ps-2 bg-light-subtle border border-black border-2 rounded-3" id="reflectionDate" name="reflectionDate" value="<?php echo date('Y-m-d H:i', strtotime($reflectionSelected['date'] ?? '')); ?>" readonly>
                     </div>
                     
                     <div class="my-3">
                         <h4 style="font-size: 30px;"><label for="title">Title</label></h4>
-                        <input type="text" class="ps-2 border border-black border-2 rounded-3" id="reflectionTitle" name="reflectionTitle" value="<?php echo $reflectionSelected['title'] ?? '' ?>" style="width:500px;">
+                        <input type="text" class="w-100 ps-2 border border-black border-2 rounded-3" id="reflectionTitle" name="reflectionTitle" value="<?php echo $reflectionSelected['title'] ?? '' ?>">
                         <div class="invalid-reflection text-danger" id="errorReflectionTitle"></div>
                     </div>
 
                     <div class="my-3">
                         <h4 style="font-size: 30px;"><label for="content">Content</label></h4>
-                        <textarea class="ps-2 border border-black border-2 rounded-3" id="reflectionContent" name="reflectionContent" style="width:500px;height:200px;"><?php echo htmlspecialchars($reflectionSelected['content'] ?? ''); ?></textarea>
+                        <textarea class="w-100 ps-2 border border-black border-2 rounded-3" id="reflectionContent" name="reflectionContent" style="height:200px;"><?php echo htmlspecialchars($reflectionSelected['content'] ?? ''); ?></textarea>
                         <div class="invalid-reflection text-danger" id="errorReflectionContent"></div>
                     </div>
 
